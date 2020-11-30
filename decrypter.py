421d425195f05d75e2aad537d429e88bdf0e99f6
sh0wer1ee <showerlee2333@gmail.com>
2020-11-30 14:04:46 +0800

    20201130

--- decrypter.py
+++ decrypter.py
@@ -92,9 +92,9 @@ def main(date, resVer, note, method):
 
 if __name__ == '__main__':
     #--Default--
-    date = '20201127'
-    resVer = 'aBYpbLFVwmiOGOrC'
-    note = '14:00 LEGEND WIND AGITO'
+    date = '20201130'
+    resVer = '5PTeAdEsB0WYRzxy'
+    note = '13:40 priconne collab'
     method = 'cs'
     #--Default--
 
