364c7d48dffa0cf8b264204dae431f2820149d65
sh0wer1ee <showerlee2333@gmail.com>
2021-02-25 11:36:29 +0800

    20210225

--- decrypter.py
+++ decrypter.py
@@ -92,9 +92,9 @@ def main(date, resVer, note, method):
 
 if __name__ == '__main__':
     #--Default--
-    date = '20210222'
-    resVer = 'gPTf6tUij0IpCaiz'
-    note = '13:40 dragon pu'
+    date = '20210225'
+    resVer = 'TkP4l5wRyWrRUUhS'
+    note = '11:00 appupdate'
     method = 'cs'
     #--Default--
 
