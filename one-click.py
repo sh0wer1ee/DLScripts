5cffaedd7cd06a082c3a3484d946bce0d895791c
sh0wer1ee <showerlee2333@gmail.com>
2021-10-20 14:04:02 +0800

    20211020-BPbvHiEm9rqXi3Gz

--- one-click.py
+++ one-click.py
@@ -3,11 +3,11 @@ import os
 
 # --CONFIG--
 proxy = 'http://127.0.0.1:10809'
-date = '20211018'  # <--
-res_ver = 'DZA20DaGt2Ihcasm'  # <--
-memo = '14:00 wind pu'  # <--
+date = '20211020'  # <--
+res_ver = 'BPbvHiEm9rqXi3Gz'  # <--
+memo = '14:00 iblis timeattack'  # <--
 new = f'prs_manifests_archive/{date}_{res_ver}'
-old = 'prs_manifests_archive/20211015_zajwQY4CyhPSR9W6'  # <--
+old = 'prs_manifests_archive/20211018_DZA20DaGt2Ihcasm'  # <--
 DIFF_ONLY = False  # <---- important
 # --CONFIG--
 
