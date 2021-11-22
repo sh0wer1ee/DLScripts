02c8c237c827d2aeabf8a074a624ec30938dc919
sh0wer1ee <showerlee2333@gmail.com>
2021-11-22 14:03:02 +0800

    20211122-Ax3w9ArEwm4mFqnZ

--- one-click.py
+++ one-click.py
@@ -3,11 +3,11 @@ import os
 
 # --CONFIG--
 proxy = 'http://127.0.0.1:10809'
-date = '20211120'  # <--
-res_ver = 'gcdMKfzbcWNVaH4p'  # <--
-memo = '00:30 surtr hp fix'  # <--
+date = '20211122'  # <--
+res_ver = 'Ax3w9ArEwm4mFqnZ'  # <--
+memo = '14:00 chapter 22 part2'  # <--
 new = f'prs_manifests_archive/{date}_{res_ver}'
-old = 'prs_manifests_archive/20211119_gy7RgJlU0GTv63SO'  # <--
+old = 'prs_manifests_archive/20211120_gcdMKfzbcWNVaH4p'  # <--
 DIFF_ONLY = False  # <---- important
 # --CONFIG--
 
